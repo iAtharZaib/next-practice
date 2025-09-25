d9856eb2c93a8aa5429957464497a7afe069d930
Athar Zaib <atharzaib@Athars-MacBook-Pro.local>
2025-09-25 15:20:52 +0400

    error handling and not found page

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -2,7 +2,6 @@ import { deleteInvoice } from '@/app/lib/action';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-
 export function CreateInvoice() {
   return (
     <Link
